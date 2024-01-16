86ee2b8d20ed8d3a271fd6a1142182cfaf576ed4
Rafael-Dallolio <106401526+Rafael-Dallolio@users.noreply.github.com>
2024-01-16 18:51:13 -0300

    Update desafio.kt
    Desafio Bootcamp DIO

--- desafio.kt
+++ desafio.kt
@@ -1,5 +1,5 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
+*** Desafio de código Kotlin***
+    
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
@@ -9,13 +9,35 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("$usuario matriculado na formação $nome.")
+    }
+
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+        println("$usuario removido da formação $nome.")
+    }
+
+    fun listarInscritos() {
+        println("Inscritos na formação $nome: $inscritos")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos")
+
+    val formacao = Formacao("Desenvolvimento Kotlin", listOf(conteudo1, conteudo2))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.listarInscritos()
+
+    formacao.desmatricular(usuario1)
+    formacao.listarInscritos()
 }
